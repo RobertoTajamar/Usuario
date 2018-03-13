ab573fa71f69536000417ba66d15408ccf871b91
RobertoTajamar <37329405+RobertoTajamar@users.noreply.github.com>
2018-03-14 00:21:26 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ProyectoValidacion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoValidacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Usuarios usuario = new Usuarios();
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Usuarios user)
+        {
+            if (ModelState.IsValid)
+            {
+                //Insercion de base datos 
+                return RedirectToAction("MensajeOK");
+            }
+            return View();
+        }
+
+        public ActionResult MensajeOK()
+        {
+            return View();
+        }
+    }
+}
