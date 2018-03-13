031da51d31fc88ba0decacaf71e44835ebd742e3
RobertoTajamar <37329405+RobertoTajamar@users.noreply.github.com>
2018-03-14 00:21:08 +0100

    Add files via upload

---
+++ Models/Usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoValidacion.Models
+{
+    public class Usuarios
+    {
+        [Required(ErrorMessage ="Campo obligatorio")]
+        [RegularExpression("[A-Za-z]*",ErrorMessage ="Solo se permite letras")]
+        public string Nombre { get; set; }
+
+        [RegularExpression("[0-9]*", ErrorMessage ="Solo permite números")]
+        [StringLength(10,ErrorMessage ="Solo diez caracteres")]
+        public string Password { get; set; }
+
+        [Range(18,90, ErrorMessage ="Solo se permiten mayores de 18")]
+        public string Edad { get; set; }
+    }
+}
